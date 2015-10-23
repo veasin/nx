ec04be4e9896591ea6fc500e4b1729c99527502a
vea <fuq@vzhen.com>
2015-10-23 17:28:47 +0800

    router[route] can config

--- router/route.php
+++ router/route.php
@@ -7,20 +7,22 @@ namespace nx\router;
  * @package nx\router
  */
 trait route{
-	private $_router_rules=[];
+	protected function nx_router_route(){
+		$this->buffer['router/route']=isset($this->setup['router/route']) ?$this->setup['router/route'] :[];
+	}
 	public function router(){
-		if(empty($this->_router_rules)) $this->control(404);
+		if(empty($this->buffer['router/route']['rules'])) $this->control(404);
 
 		$method=strtolower($_SERVER['REQUEST_METHOD']);
 		$uri=isset($_SERVER['PATH_INFO']) ?$_SERVER['PATH_INFO'] :$_SERVER['QUERY_STRING'];
 
-		foreach($this->_router_rules as $route){
-			$_match_method=($route['method']==$method || $route['method']=='*');
+		foreach($this->buffer['router/route']['rules'] as $route){
+			$_match_method=($route[0]==$method || $route[0]=='*');
 			//如果没有匹配直接继续下一个
 			if(!$_match_method) continue;
 
 			$_match_path=false;
-			$path=$route['route'];
+			$path=$route[1];
 			$un=isset($path[0]) && $path[0]=='!';
 			$i=$un ?1 :0;
 			$params=[];
@@ -36,25 +38,26 @@ trait route{
 			if($_match_path){
 				if(isset($params[0])) array_shift($params);
 				$this->request['params']=$params;
-
-				$result=call_user_func_array($route['callback'] ?$route['callback'] :[$this, 'control'], [$this->request, $this->response, $this]);
+				$is_controller=($route[2]===false);
+				$result=call_user_func_array($is_controller ?[$this, 'control'] :$route[2],
+					$is_controller ?[$route[3]] :[$this->request, $this->response, $this]);
 				if(!is_null($result)) return $result;
 			}
 		}
 	}
-	public function on($route, $callback){
-		$this->_router_rules[]=['method'=>'*', 'route'=>$route, 'callback'=>$callback];
+	public function on($route, $callback, $control=[]){
+		$this->buffer['router/route']['rules'][]=['*', $route, $callback, $control];
 	}
-	public function get($route, $callback){
-		$this->_router_rules[]=['method'=>'get', 'route'=>$route, 'callback'=>$callback];
+	public function get($route, $callback, $control=[]){
+		$this->buffer['router/route']['rules'][]=['get', $route, $callback, $control];
 	}
-	public function post($route, $callback){
-		$this->_router_rules[]=['method'=>'post', 'route'=>$route, 'callback'=>$callback];
+	public function post($route, $callback, $control=[]){
+		$this->buffer['router/route']['rules'][]=['post', $route, $callback, $control];
 	}
-	public function put($route, $callback){
-		$this->_router_rules[]=['method'=>'put', 'route'=>$route, 'callback'=>$callback];
+	public function put($route, $callback, $control=[]){
+		$this->buffer['router/route']['rules'][]=['put', $route, $callback, $control];
 	}
-	public function delete($route, $callback){
-		$this->_router_rules[]=['method'=>'delete', 'route'=>$route, 'callback'=>$callback];
+	public function delete($route, $callback, $control=[]){
+		$this->buffer['router/route']['rules'][]=['delete', $route, $callback, $control];
 	}
 }
