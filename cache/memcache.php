0e04dd4b224140f795d05223e843cb51e9902b5b
vea <fuq@vzhen.com>
2015-12-03 10:25:59 +0800

    trait memcache fix

--- cache/memcache.php
+++ cache/memcache.php
@@ -15,17 +15,22 @@ namespace nx\cache;
 trait memcache{
 	protected function nx_cache_memcache(){
 		$it=is_a($this, 'nx\app') ?$this :$this->app;
-		if(!isset($this->buffer['cache/memcache'])) $this->buffer['cache/memcache']=['config'=>isset($it->setup['cache/memcache']) ?$it->setup['cache/memcache'] :[], 'handle'=>[],];
+		if(!isset($this->buffer['cache/memcache'])){
+			$this->buffer['cache/memcache']=['config'=>isset($it->setup['cache/memcache']) ?$it->setup['cache/memcache'] :[], 'handle'=>[],];
+		}
 	}
+	/**
+	 * @param string $name
+	 * @return \Memcache
+	 */
 	public function cache($name='default'){
 		$cache=&$this->buffer['cache/memcache']['handle'];
 		if(!isset($cache[$name])){
 			$cfg=&$this->buffer['cache/memcache']['config'];
 			$config=false;
 			if(isset($cfg[$name])) $config=is_array($cfg[$name]) ?$cfg[$name] :$cfg[$cfg[$name]];
-			$cache[$name]=empty($config)
-				?new \memcache()
-				:new \memcache($config['host'], isset($config['host']) ?$config['host'] :11211, isset($config['timeout']) ?$config['timeout'] :1);
+			$cache[$name] = new \Memcache();
+			if(!empty($config)) $cache[$name]->connect($config['host'], isset($config['port']) ?$config['port'] :11211, isset($config['timeout']) ?$config['timeout'] :1);
 		}
 		return $cache[$name];
 	}
