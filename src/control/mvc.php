008458e07e5f1d2a1c396e38cd688083932c2ab7
vea <vea@live.cn>
2016-08-22 17:43:45 +0800

    control namespace fix

--- src/control/mvc.php
+++ src/control/mvc.php
@@ -10,8 +10,8 @@ trait mvc{
 	public function control($route){
 		$this->log('control(mvc):'.json_encode($route));
 		if(isset($route[0]) && is_string($route[0])){
-			list($namespace,) =explode('\\', get_class($this), 2);
-			$_controller =$namespace.'\controllers\\'.$route[0];
+			$class =get_class($this);
+			$_controller =substr($class, 0, strrpos($class, '\\')).'\controllers\\'.$route[0];
 			$this->log(' - '.$_controller);
 			return class_exists($_controller, true)? new $_controller($route, $this):new \nx\mvc\controller($route, $this);
 		} elseif(is_callable($route)) return call_user_func($route, $this->resquest, $this->response);
