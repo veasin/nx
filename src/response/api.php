6e54fd28cd0a69f781785308f2aa1d962ca928f6
vea <vea@live.cn>
2016-10-09 13:46:53 +0800

    response api json header

--- src/response/api.php
+++ src/response/api.php
@@ -8,6 +8,7 @@ trait api{
 	protected function nx_response_api(){
 		$it = is_a($this, 'nx\app') ?$this :$this->app;
 		$it->response = new \nx\response();
+		$it->response->contentType('application/json', 'utf-8');
 	}
 	/**
 	 * @param string $uri
@@ -15,7 +16,8 @@ trait api{
 	 * @return mixed
 	 */
 	public function redirect($uri = '', $code =302){
-		return $this->response->status(302, '', false)->redirect($uri);
+		$it = is_a($this, 'nx\app') ?$this :$this->app;
+		return $it->response->status(302, '', false)->redirect($uri);
 	}
 	/**
 	 * fn(code, msg), fn(code, data), fn(data), fn(code)
@@ -31,14 +33,14 @@ trait api{
 			else $_data =['err'=>0, 'data'=>$code];
 		}else{
 			$_data['err'] = $code;
-			if(isset($this->response->status[$code])) $_data['msg'] = $it->i18n($this->response->status[$code]);
+			if(isset($it->response->status[$code])) $_data['msg'] = $it->i18n($it->response->status[$code]);
 			if($code ==0){
 				if(func_num_args() >1) $_data['data'] =$data;
-				elseif(is_null($data)) $_data['data'] =$this->response->data();
+				elseif(is_null($data)) $_data['data'] =$it->response->data();
 			}
 			elseif(is_string($data)) $_data['msg'] = $data;
 		}
-		$this->response->set($_data);
+		$it->response->set($_data);
 		if($die) die();
 		return false;
 	}
