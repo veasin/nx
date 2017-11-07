ccd46d27178253c5b27b97b44538c672452c7e64
vea <vea@live.cn>
2017-11-07 18:02:48 +0800

    trait config rebulid & add path of files

--- src/config/files.php
+++ src/config/files.php
@@ -3,42 +3,37 @@ namespace nx\config;
 
 /**
  * Trait files
- * @trait app
+ * @trait   app
  * @package nx\config
  */
 trait files{
 	protected function nx_config_files(){
-		!isset($this->config) && $this->config =[];
-		if(isset($this->buffer)){
-			if(!isset($this->buffer['config'])) $this->buffer['config'] =[];
-		}
+		$it=is_a($this, 'nx\app') ?$this :$this->app;
+		$it->buffer['config/files']=$it->setup['config/files'] ?? [];
+		$it->buffer['config/files']['path']=$it->buffer['config/files']['path'] ?? $it->path.'/config/';
+		$it->buffer['config/files']['cache']=$it->setup['config'] ?? [];
 	}
-
-	public function config($word, $params =null){
-		if(isset($this->config[$word])) return $this->config[$word];
-
-		$_ns =$word;
-		$_key =null;
-		if(strpos($word, '.') !== false) list($_ns, $_key) =explode('.', $word, 2);
-
-		$buffer =&$this->buffer['config'];
-		if(!isset($buffer[$_ns])){
-			$config = [];
-			if(is_file($file =$this->path.'/config/'.$_ns.'.php')){
-				$config =include($file);
+	/**
+	 * 读取配置内容
+	 * @param      $word   "ns.key"
+	 * @param null $params 默认值
+	 * @return null
+	 */
+	public function config($word, $params=null){
+		$it=is_a($this, 'nx\app') ?$this :$this->app;
+		if(array_key_exists($word, $it->config)) return $it->config[$word];
+		$_ns=$word;
+		$_key=null;
+		if(false !== strpos($word, '.')) list($_ns, $_key)=explode('.', $word, 2);
+		$buffer=&$it->buffer['config/files']['cache'];
+		if(!array_key_exists($_ns, $buffer)){
+			$config=[];
+			if(is_file($file=$it->buffer['config/files']['path'].$_ns.'.php')){
+				$config=@include($file);
 			}
-			$buffer[$_ns] =$config;
+			$buffer[$_ns]=$config;
 		}
-
-		$_config =$params;
-		if(isset($buffer[$_ns])){
-			if(is_null($_key)) $_config =$buffer[$_ns];
-			elseif(isset($buffer[$_ns][$_key])) $_config =$buffer[$_ns][$_key];
-		}
-
-		$this->config[$word] =$_config;
-		return $_config;
+		$it->config[$word]=is_null($_key) ?$buffer[$_ns] :(isset($buffer[$_ns][$_key]) ?$buffer[$_ns][$_key] :$params);
+		return $it->config[$word];
 	}
-
-
 }
