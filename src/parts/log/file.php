ec3ae38747700e6b5c6d938281683ab84b4985ec
Vea <vea.urn2@gmail.com>
2020-06-23 17:14:40 +0800

    parts\log\file 重构 1.1.3

--- src/parts/log/file.php
+++ src/parts/log/file.php
@@ -4,54 +4,36 @@ namespace nx\parts\log;
 trait file{
 	protected function nx_parts_log_file(){
 		$setup=$this->setup['log/file'] ?? [];
-		$name=date($setup['name'] ?? 'Y-m-d');
-		$path=$setup['path'] ?? $this->getPath('./logs/');
-		$file=$path.$name.'.log';
 		$this->buffer['log/file']=[
-			'file'=>$file,
 			'line'=>$setup['line'] ?? '[{micro+}] {var}',
-			'handle'=>@fopen($file, 'a'),
 			'start'=>time(),
 			'start-micro'=>microtime(true),
 			'method'=>$_SERVER['REQUEST_METHOD'],
 			'uri'=>$_SERVER['REQUEST_URI'],
+			'list'=>[],
 		];
-		$this->log('{datetime}:[{method}]{uri}', '{var}');
+		$date =date('Y-m-d H:i:s');
+		$this->log("{$date} {$_SERVER['REQUEST_METHOD']}:{$_SERVER['REQUEST_URI']}");
 
 		yield;
+
 		$this->log("end.\n");
+
+		$path=$setup['path'] ?? $this->getPath('./logs/');
+		$name=date($setup['name'] ?? 'Y-m-d');
+		$handle =@fopen($path.$name.'.log', 'a');
+		if($handle){
+			fwrite($handle, implode("\n", $this->buffer['log/file']['list'])."\n");
+			fclose($handle);
+		}
 	}
 	/**
 	 * @param      $var
 	 * @param bool $template
 	 */
 	public function log($var, $template=null){
-		$buffer=&$this->buffer['log/file'];
-		if(empty($buffer['handle'])) return;
-		//$template=$template ?$template :$this->buffer['log/file']['line'];
-		//if($onlyvar) return fwrite($this->buffer['log/file']['handle'], $var."\n");
 		if(!is_string($var)) $var=json_encode($var, JSON_UNESCAPED_UNICODE);
-		$line=str_replace([
-			'{var}',
-			'{time}',
-			'{datetime}',
-			'{microtime}',
-			'{create}',
-			//'{app}',
-			'{method}',
-			'{uri}',
-			'{micro+}',
-		], [
-			$var,
-			date('H:i:s'),
-			date('Y-m-d H:i:s'),
-			microtime(true),
-			time(),
-			//__CLASS__,
-			$buffer['method'],
-			$buffer['uri'],
-			sprintf("%06.2fms", (microtime(true) - $buffer['start-micro']) * 1000),
-		], "{$this->getUUID()} ".($template ?? $buffer['line']));
-		fwrite($buffer['handle'], $line."\n");
+		$step =sprintf("%06.2fms", (microtime(true) - $this->buffer['log/file']['start-micro']) * 1000);
+		$this->buffer['log/file']['list'][] ="{$this->getUUID()} [{$step}] {$var}";
 	}
 }
