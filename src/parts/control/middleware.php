ddb450105fc86b7b42b22fa9242b2040308d2c82
Vea <vea.fu@vertuonline.com>
2020-10-21 19:49:44 +0800

    1.1.6 移除callable数组支持, 支持绝对路径 \controller 控制器写法

--- src/parts/control/middleware.php
+++ src/parts/control/middleware.php
@@ -15,14 +15,16 @@ trait middleware{
 	 * @return mixed
 	 */
 	public function control($call, ...$args){
-		if(is_callable($call)){//['\api\user','check'],[$this, 'main'],
-			return call_user_func_array($call, $args);
-		}elseif(isset($call[1]) && $call[1] instanceof \Closure){//[null, function(){}],
+		if(isset($call[1]) && $call[1] instanceof \Closure){//[null, function(){}],
 			return call_user_func_array($call[1]->bindTo($call[0] ?? $this), $args);
 		}else{
 			if(isset($call[0]) && isset($call[1]) && is_string($call[0]) && is_string($call[1])){//['user','check'],
-				$pos =strrpos(__CLASS__, '\\');
-				$name=substr(__CLASS__, 0, $pos?$pos:strlen(__CLASS__)).'\controllers\\'.$call[0];
+				if('\\'===$call[0][0]){
+					$name=$call[0];
+				} else{
+					$pos=strrpos(__CLASS__, '\\');
+					$name=substr(__CLASS__, 0, $pos ?$pos :strlen(__CLASS__)).'\controllers\\'.$call[0];
+				}
 				if(!array_key_exists($name, $this->cacheControllers)) $this->cacheControllers[$name] =class_exists($name, true) ?new $name($this) :null;
 				if($this->cacheControllers[$name] ?? false){
 					$exists =method_exists($this->cacheControllers[$name], $call[1]);
